715f55681ea0cf7545e98ab7b8cd47b888a4de9f
Brandon Craig <twilightraven13@yahoo.com>
2020-12-19 23:28:10 -0800

    Added Bootstrap cards to the search result and the 5-day forecast. Search history displays a list. Only 1 day shows up for the 5-day forecast. Need the other 4 to display inline.

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 var apiKey = "2788d55befd914b1820a7e359663b5fd";
 var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchVal + "&units=imperial&appid=" + apiKey;
-var searchVal = $("#search").text();
+var searchVal = $("#searchtext").val();
 
 $("#searchbtn").on("click", function() {
     searchBtn();
 });
 
 function searchBtn() {
-    console.log($("#searchtext").val());
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+      }).then(function (data) {
+      //Display the following in the .result div--
+      
+      //City name (current date) in larger text
+      //Temp in F
+      //Humidity in %
+      //Wind Speed in mph
+      //skip UV index for now
+
+      //5 day forecast will go in a row here
+    
+    
+    
+    });
+    
+    // $("searchtext").clear(); --clear form?
 }
+
+
 
