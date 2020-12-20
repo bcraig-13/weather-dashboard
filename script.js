966f951c0e7b28ecf687cb22f0c3697ffa921d00
Brandon Craig <twilightraven13@yahoo.com>
2020-12-20 02:00:21 -0800

    The search button now prepends the value to the list below it and clears the input box upon search button press.

--- script.js
+++ script.js
@@ -2,31 +2,39 @@ var apiKey = "2788d55befd914b1820a7e359663b5fd";
 var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchVal + "&units=imperial&appid=" + apiKey;
 var searchVal = $("#searchtext").val();
 
-$("#searchbtn").on("click", function() {
+$("#searchbtn").on("click", function () {
     searchBtn();
 });
 
 function searchBtn() {
-    $.ajax({
-        url: queryUrl,
-        method: "GET",
-      }).then(function (data) {
-      //Display the following in the .result div--
-      
-      //City name (current date) in larger text
-      //Temp in F
-      //Humidity in %
-      //Wind Speed in mph
-      //skip UV index for now
+    var searchVal = $("#searchtext").val();
+    $("#searchtext").val("");
+    $("#history").prepend("<li class='list-group-item'>" + searchVal + "</li>");
+    
+    // $.ajax({
+    //     url: queryUrl,
+    //     method: "GET",
+    // }).then(function (data) {
+    //     //Display the following in the .result div--
 
-      //5 day forecast will go in a row here
-    
-    
-    
-    });
-    
+    //     //City name in h3
+    //     //Temp in F
+    //     //Humidity in %
+    //     //Wind Speed in mph
+    //     //5 day forecast will go in a row here
+    //     //City will get added to top list group item
+        
+    //     //Previous search will be pushed down
+
+
+
+
+    // });
+
     // $("searchtext").clear(); --clear form?
 }
+
+
 
 
 
