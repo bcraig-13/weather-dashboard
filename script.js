14041653d42f45ea36fcdbc7992eac1289d13fb8
Brandon Craig <thegamingraven13@gmail.com>
2020-12-21 17:54:06 -0800

    5-day forecast works. Day 5 isn't displaying cause it might bee too far out for the api to read.

--- script.js
+++ script.js
@@ -40,16 +40,6 @@ function searchBtn() {
     $("#mainTemp").text(data.main.temp + "°");
     $("#mainHum").text(data.main.humidity + " %");
     $("#mainWs").text(data.wind.speed + " mph");
-    //5 day forecast will go in a row here
-    //DAY 1
-
-    //DAY 2
-
-    //DAY 3
-
-    //DAY 4
-
-    //DAY 5
   });
 }
 
@@ -58,25 +48,72 @@ function forecast() {
     var searchVal = $("#searchtext").val();
     var forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchVal + "&appid=" + apiKey;
     
+    $("#searchtext").val("");
 
     $.ajax({
         url: forecast,
         method: "GET",
     }).then(function (data) {
+        
+        //DAY 1
         var dayOneEl = moment().add(1, "days").format("M/D");
         var dayOneIcon = data.list[8].weather[0].icon;
         var dayOneImg = $("<img>").attr("src", "http://openweathermap.org/img/w/" + dayOneIcon + ".png");
         var dayOneTemp = data.list[8].main.temp;
         var dayOneHum = data.list[8].main.humidity;
         
-
         $(".day-one-date").append(dayOneEl);
         $("#day-one-img").append(dayOneImg);
-        $("#day-one-temp").append(dayOneTemp);
-        $("#day-one-hum").append(dayOneHum);
+        $("#day-one-temp").append(dayOneTemp + "°");
+        $("#day-one-hum").append(dayOneHum + " %");
+        //DAY 2
+        var dayTwoEl = moment().add(2, "days").format("M/D");
+        var dayTwoIcon = data.list[16].weather[0].icon;
+        var dayTwoImg = $("<img>").attr("src", "http://openweathermap.org/img/w/" + dayTwoIcon + ".png");
+        var dayTwoTemp = data.list[16].main.temp;
+        var dayTwoHum = data.list[16].main.humidity;
+        
+        $(".day-two-date").append(dayTwoEl);
+        $("#day-two-img").append(dayTwoImg);
+        $("#day-two-temp").append(dayTwoTemp + "°");
+        $("#day-two-hum").append(dayTwoHum + " %");
+        //DAY 3
+        var dayThreeEl = moment().add(3, "days").format("M/D");
+        var dayThreeIcon = data.list[24].weather[0].icon;
+        var dayThreeImg = $("<img>").attr("src", "http://openweathermap.org/img/w/" + dayThreeIcon + ".png");
+        var dayThreeTemp = data.list[24].main.temp;
+        var dayThreeHum = data.list[24].main.humidity;
+        
+        $(".day-three-date").append(dayThreeEl);
+        $("#day-three-img").append(dayThreeImg);
+        $("#day-three-temp").append(dayThreeTemp + "°");
+        $("#day-three-hum").append(dayThreeHum + " %");
+        //DAY 4
+        var dayFourEl = moment().add(4, "days").format("M/D");
+        var dayFourIcon = data.list[32].weather[0].icon;
+        var dayFourImg = $("<img>").attr("src", "http://openweathermap.org/img/w/" + dayFourIcon + ".png");
+        var dayFourTemp = data.list[32].main.temp;
+        var dayFourHum = data.list[32].main.humidity;
+        
+        $(".day-four-date").append(dayFourEl);
+        $("#day-four-img").append(dayFourImg);
+        $("#day-four-temp").append(dayFourTemp + "°");
+        $("#day-four-hum").append(dayFourHum + " %");
+        //DAY 5 - NOT READING IN OPEN WEATHER API
+        
 
+        // var dayFiveEl = moment().add(5, "days").format("M/D");
+        // var dayFiveIcon = data.list[40].weather[0].icon;
+        // var dayFiveImg = $("<img>").attr("src", "http://openweathermap.org/img/w/" + dayFiveIcon + ".png");
+        // var dayFiveTemp = data.list[40].main.temp;
+        // var dayFiveHum = data.list[40].main.humidity;
+        
+        // $(".day-five-date").append(dayFiveEl);
+        // $("#day-five-img").append(dayFiveImg);
+        // $("#day-five-temp").append(dayFiveTemp + "°");
+        // $("#day-five-hum").append(dayFiveHum + " %");
 
         //put this at the end
-        // $("#searchtext").val("");
+        
     });
 }
